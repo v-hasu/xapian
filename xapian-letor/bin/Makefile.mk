21d0f33b9688320a6f58f1a01fa3a0219afd767a
v-hasu <sunhanxiaoisme@gmail.com>
2014-03-24 08:43:03 -0700

    mv the questletor.1 and xapian-letor-update.cc.1 to questletor.1 and xapian-letor-update.cc

--- xapian-letor/bin/Makefile.mk
+++ xapian-letor/bin/Makefile.mk
@@ -16,14 +16,14 @@ bin_xapian_letor_update_LDADD = $(XAPIAN_LIBS)
 
 if !MAINTAINER_NO_DOCS
 dist_man_MANS +=\
-	bin/questletor.1\
-	bin/xapian-letor-update.1
+	bin/questletor\
+	bin/xapian-letor-update
 endif
 
 if DOCUMENTATION_RULES
-bin/questletor.1: bin/questletor$(EXEEXT) makemanpage
-	./makemanpage bin/questletor $(srcdir)/bin/questletor.cc bin/questletor.1
+bin/questletor: bin/questletor$(EXEEXT) makemanpage
+	./makemanpage bin/questletor $(srcdir)/bin/questletor.cc bin/questletor
 
-bin/xapian-letor-update.1: bin/xapian-letor-update$(EXEEXT) makemanpage
-	./makemanpage bin/xapian-letor-update $(srcdir)/bin/xapian-letor-update.cc bin/xapian-letor-update.1
+bin/xapian-letor-update: bin/xapian-letor-update$(EXEEXT) makemanpage
+	./makemanpage bin/xapian-letor-update $(srcdir)/bin/xapian-letor-update.cc bin/xapian-letor-update
 endif
