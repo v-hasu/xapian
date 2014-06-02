118d74a29ec1e9847a907842c21e129ec3cc41cc
v-hasu <sunhanxiaoisme@gmail.com>
2014-06-02 08:04:09 -0700

    delete all the test code and fix some bugs as James's comments

--- xapian-letor/scorer.cc
+++ xapian-letor/scorer.cc
@@ -1,5 +1,5 @@
 #include <xapian.h>
-#include <xapian/intrusive_ptr.h>           //#include <xapian/base.h>
+#include <xapian/intrusive_ptr.h>           
 #include <xapian/types.h>
 #include <xapian/visibility.h>
 
@@ -17,17 +17,19 @@ using namespace Xapian;
 Scorer::Scorer() {
 }
 
-static double get_dcg(std::vector<double> lables){
+static double 
+get_dcg(std::vector<double> lables){
 
-	//int test[6]={3,2,3,0,1,2};//test code
 	int length = lables.size();
-
 	double dcg = 0.0;//need error processing
-	if (lables.empty()!=1)
-		 dcg = lables[0];//test[0];
-	else
+
+	if (lables.empty()!=1){
+		dcg = lables[0];
+	}
+	else{
 		std::cout<<"lables in scorer is empty!"<<endl;
-	
+	}	
+
 	for (int i = 1; i <length; ++i){
 		dcg += lables[i]/(log(i+1)/log(2));
 	}
@@ -36,78 +38,75 @@ static double get_dcg(std::vector<double> lables){
 
 }
 
-double
-Scorer::ndcg_scorer(Xapian::RankList & rl){//NGCD first
+static std::vector<double>
+get_lables(Xapian::RankList rl){
 
 	std::vector<Xapian::FeatureVector> fvv = rl.get_data();
 	int fvvsize = fvv.size();
 	std::vector<double> lables;
-	/*
-	 *test date from http://en.wikipedia.org/wiki/Discounted_cumulative_gain
-	 *should return a ERR score 0.63
-	lables.push_back(3.0);
-	lables.push_back(2.0);
-	lables.push_back(3.0);
-	lables.push_back(0.0);
-	lables.push_back(1.0);
-	lables.push_back(2.0);
-	*/
 	
 	for (int i = 0; i <fvvsize; ++i){
 		lables.push_back(fvv[i].get_label());
 	}
 
+	return lables;
+
+}
+
+/*
+test the ndcg_scorer() use the data from http://en.wikipedia.org/wiki/Discounted_cumulative_gain
+input:[3,2,3,0,1,2]
+output:0.932
+*/
+double
+Scorer::ndcg_scorer(Xapian::RankList & rl){
+
+	std::vector<double> lables = get_lables(rl);
+
+	//DCG score of original ranking
 	double DCG = get_dcg(lables);
-	sort(lables.begin(),lables.begin()+fvvsize,std::greater<int>());
+
+	//DCG score of ideal ranking
+	sort(lables.begin(),lables.begin()+lables.size(),std::greater<int>());
 	double iDCG = get_dcg(lables);
-	/*
-	std::cout<<"DCG"<<DCG<<endl;
-	std::cout<<"iDCG"<<iDCG<<endl;
-	std::cout<<"ndcg"<<DCG/iDCG<<endl;
-	*/
+
 	return DCG/iDCG;
 
 }
 
+/*
+test the err_scorer() use the data from http://lingpipe-blog.com/2010/03/09/chapelle-metzler-zhang-grinspan-2009-expected-reciprocal-rank-for-graded-relevance/
+input:[3,2,4]
+output:0.63
+*/
 double
 Scorer::err_scorer(Xapian::RankList & rl){
-	//need to be test
-	int MAX_LABEL = 16; //hard code first, meaning 2^(5-1)
 
-	std::vector<Xapian::FeatureVector> fvv = rl.get_data();
-	int fvvsize = //fvv.size();
-	std::vector<double> lables;
+	//hard code for the a five-point scale, the 16 means 2^(5-1)
+	int MAX_LABEL = 16; 
 
-	/*
-	 *test date from http://lingpipe-blog.com/2010/03/09/chapelle-metzler-zhang-grinspan-2009-expected-reciprocal-rank-for-graded-relevance/
-	 *should return a ERR score 0.63
-	lables.push_back(3.0);
-	lables.push_back(2.0);
-	lables.push_back(4.0);
-	*/
+	std::vector<double> lables = get_lables(rl);
+	int length = lables.size();
 
-	for (int i = 0; i <fvvsize; ++i){
-		lables[i] = (pow(2,lables[i])-1)/MAX_LABEL;//fvv[i].get_label()
-		//std::cout<<"lables:"<<lables[i]<<endl;
+	//compute the satisfaction probability for lable of each doc in the ranking
+	for (int i = 0; i <length; ++i){
+		lables[i] = (pow(2,lables[i])-1)/MAX_LABEL;
 	}
 
 	double err_score = lables[0];
-	//std::cout<<"ERR:"<<err_score<<endl;
 
-	for (int i = 1; i <fvvsize; ++i){
+	//compute the accumulated probability for each doc which user will stop at
+	for (int i = 1; i <length; ++i){
+
+		//single stop probability
 		double temp_err = (1.0/(i+1.0))*lables[i];
-		//std::cout<<"lables:"<<lables[i]<<endl;
-		//std::cout<<"temp_err:"<<temp_err<<endl;
-		for (int j=i-1; j>=0; --j)
+
+		//for user 
+		for (int j=i-1; j>=0; --j){
 			temp_err *= (1-lables[j]);
-		//std::cout<<"temp_err:"<<temp_err<<endl;
+		}
 		err_score += temp_err;
-		//std::cout<<"ERR:"<<err_score<<endl;
 	}
-	//std::cout<<"ERR:"<<err_score<<endl;
 	return err_score;
 
 }
-
-
-l
